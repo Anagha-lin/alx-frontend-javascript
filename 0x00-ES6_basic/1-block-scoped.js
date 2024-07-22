1598e71ba3053d9ead630ffb275644dc184c0df9
Anagha-lin <linusanagha37@gmail.com>
2024-07-22 23:44:12 +0000

    a

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,16 +1,14 @@
+// 1-block-scoped.js
+
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-<<<<<<< HEAD
-     const task = true;
-    var task2 = false;
-=======
-    const task = true; // eslint-disable-line no-unused-vars
-    const task2 = false; // eslint-disable-line no-unused-vars
->>>>>>> 64880386096f1a85c19e24341a7feadf10460990
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
 }
+
